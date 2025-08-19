04f92c78e0be1837b5c48f3f0edec9175f2eb624
Albert <albert.nigmatullin@simbirsoft.com>
2025-08-19 14:53:52 +0300

    added text box tests

---
+++ src/test/java/com/simbirsoft/tests/TextBoxTests.java
@@ -0,0 +1,37 @@
+package com.simbirsoft.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        Configuration.browserSize = screenSize.width + "x" + screenSize.height;
+        Configuration.pageLoadTimeout = 60000;
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest(){
+        String permanentAddress = "some street 2";
+
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@qaguru.com");
+        $("#currentAddress").setValue("some street 1");
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@qaguru.com"));
+        $("#output #currentAddress").shouldHave(text("some street 1"));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+
+    }
+}
