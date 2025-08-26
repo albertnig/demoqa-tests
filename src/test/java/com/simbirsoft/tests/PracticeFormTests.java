4305c023c23088a2dff25a427d469b993175cf70
Albert <albert.nigmatullin@simbirsoft.com>
2025-08-26 21:40:29 +0300

    fix

--- src/test/java/com/simbirsoft/tests/PracticeFormTests.java
+++ src/test/java/com/simbirsoft/tests/PracticeFormTests.java
@@ -3,7 +3,6 @@ package com.simbirsoft.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
