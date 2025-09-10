534928fd9d77dc9b50b3ea1a76c286cedde8ab90
Albert <albert.nigmatullin@simbirsoft.com>
2025-09-10 22:20:59 +0300

    add Alfa & Tbank

---
+++ src/test/java/com/simbirsoft/tests/TbankTest.java
@@ -0,0 +1,19 @@
+package com.simbirsoft.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TbankTest {
+    @Test
+    void testTbank(){
+        open("https://www.tbank.ru/");
+        $("body").shouldHave(exactText("Открыть вклад")).click();
+        $("body").shouldHave(text("Настройте вклад"));
+        $$(byText("В юанях")).find(visible).click();
+        //$("body").shouldHave(text("Накопительный Альфа‑Счёт"));
+    }
+}
