f9e1f01aac228cbbdb0e48036e9826d97938258d
Albert <albert.nigmatullin@simbirsoft.com>
2025-09-11 22:25:45 +0300

    add Alfa & Tbank

--- src/test/java/com/simbirsoft/tests/TbankTest.java
+++ src/test/java/com/simbirsoft/tests/TbankTest.java
@@ -1,7 +1,6 @@
 package com.simbirsoft.tests;
 
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
