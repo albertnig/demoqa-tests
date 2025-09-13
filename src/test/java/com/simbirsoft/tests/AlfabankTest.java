d1e74cf2fc331232cbd074e6325580b63c2c9689
Albert <albert.nigmatullin@simbirsoft.com>
2025-09-13 22:59:34 +0300

    fix Alfa

--- src/test/java/com/simbirsoft/tests/AlfabankTest.java
+++ src/test/java/com/simbirsoft/tests/AlfabankTest.java
@@ -1,7 +1,6 @@
 package com.simbirsoft.tests;
 
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
