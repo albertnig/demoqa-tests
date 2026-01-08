5b88e55dc958a11bd888787b0623e37aa9495d63
Albert <albert.nigmatullin@simbirsoft.com>
2026-01-09 01:32:45 +0300

    Fix

--- src/test/java/com/simbirsoft/tests/vasenkoff/topic5/classwork/pages/components/ResultsModal.java
+++ src/test/java/com/simbirsoft/tests/vasenkoff/topic5/classwork/pages/components/ResultsModal.java
@@ -2,9 +2,7 @@ package com.simbirsoft.tests.vasenkoff.topic5.classwork.pages.components;
 
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.conditions.Visible;
-
 import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
